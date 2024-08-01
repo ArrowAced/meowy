f29887c74bba44bd77d7d056e52b6a70c392d540
Arrow <89811520+ArrowAced@users.noreply.github.com>
2024-08-01 14:00:18 -0400

    meow :3

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import "@std/dotenv/load";
 import { RoarBot } from "@mbw/roarbot"
+
 const Meowy = new RoarBot({
     admins: JSON.parse(Deno.env.get("MEOWY_ADMINS")!),
     help: false
@@ -9,6 +10,13 @@ Meowy.command("help", {
     args: [],
     fn: (reply) => {
         reply("Commands:\nhelp - You know what this does.")
+    }
+})
+
+Meowy.command("meow", {
+    args: [],
+    fn: (reply) => {
+        reply("meow :3")
     }
 })
 
